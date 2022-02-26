075f350bb53a16cb555a7f09c525913dc7684b86
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-02-26 22:14:25 +0100

    Add isXHR method to Request

--- src/Request.php
+++ src/Request.php
@@ -7,4 +7,9 @@ use Chuck\Request as BaseRequest;
 
 class Request extends BaseRequest
 {
+    public function isXHR(): bool
+    {
+        return (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+            strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+    }
 }
