002d515a74402ee13d277a22e6df73e9f824c36f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-02-26 22:13:47 +0100

    Add nanoid util function

---
+++ src/Util/nanoid.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Util;
+
+use Hidehalo\Nanoid\Client;
+
+if (!function_exists('Conia\Util\nanoid')) {
+
+    function nanoid()
+    {
+        $client = new Client();
+
+        return $client->formattedId(
+            alphabet: '123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz',
+            size: 13
+        );
+    }
+}
