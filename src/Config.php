b0d9aceb04803c93586ee4b4ff01736ab86db6f5
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-02-26 22:16:00 +0100

    Initialize config with db credentials via run script

--- src/Config.php
+++ src/Config.php
@@ -5,20 +5,16 @@ declare(strict_types=1);
 namespace Conia;
 
 use Chuck\Config as BaseConfig;
-use Conia\Model\Auth;
+
 
 class Config extends BaseConfig
 {
     public function __construct(array $config)
     {
-        $cmsConfig = [
-            'di' => [
-                'Auth' => Auth::class,
-            ],
-        ];
+        $coniaConfig = [];
 
         parent::__construct(array_replace_recursive(
-            $cmsConfig,
+            $coniaConfig,
             $config,
         ));
     }
