d2a9d9226a2cc1c0fd1c704c40df2dc8184c2f4c
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-03-06 20:35:50 +0100

    Add String util

---
+++ src/Util/Strings.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Util;
+
+class Strings
+{
+    /**
+     * Calculate entropy of a string
+     */
+    public static function entropy(string $str): float
+    {
+        $classes = [
+            // lower case uncode characters
+            '/\p{Ll}/',
+            // upper case uncode characters
+            '/\p{Lu}/',
+            // unicode numbers
+            '/\p{N}/',
+        ];
+
+        $size = 0.0;
+        $str = trim($str);
+        $len = strlen($str);
+        $classCount = 0;
+
+        foreach ($classes as $pattern) {
+            $matches = [];
+            if (preg_match_all($pattern, $str, $matches)) {
+                $size += count(array_unique($matches[0]));
+                $classCount += 1;
+            }
+        }
+
+        // special characters
+        $matches = [];
+        foreach (str_split("/[ ,.?!\"┬ú$%^&*()-_=+[]{};:'@#~<>/\\|`┬¼┬ª]/", 1) as $char) {
+            if (strpos($str, $char) !== false) {
+                $matches[] = $char;
+            }
+        }
+        $foundSpecialChars = count(array_unique($matches));
+        if ($foundSpecialChars > 0) {
+            $classCount += 1;
+            $size += $foundSpecialChars;
+        }
+
+        // Evaluate if its a simple string of chars next to each other
+        //   Like: abcdef or 1234
+        // This is only an approximation an should not add too much weight
+        // If this is below certain thresholds
+        $sumDiff = 1;
+        $chars = str_split($str, 1);
+        for ($i = 1; $i < count($chars); $i++) {
+            $sumDiff += abs(mb_ord($chars[$i - 1]) - mb_ord($chars[$i]));
+        }
+
+        // probably something like acegik...
+        if ($sumDiff <= 12) {
+            $len -= 1;
+        }
+        // probably something like 12345 or aaabbb
+        if ($sumDiff <= 5) {
+            $len -= 1;
+        }
+
+        if ($classCount > 0) {
+            $size += $classCount - 1;
+        }
+
+        if ($size === 0.0 || $len <= 0) {
+            return 0;
+        }
+        return log($size, 2) * $len;
+    }
+}
---
+++ tests/UtilStringTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Conia\Util\Strings;
+
+
+test('String entropy', function () {
+    $lower = Strings::entropy('spirit crusher');
+    $upper = Strings::entropy('SPIRIT CRUSHER');
+    $mixed = Strings::entropy('Spirit Crusher');
+
+    expect($lower)->toBe($upper);
+    expect($lower)->toBeLessThan($mixed);
+    expect(Strings::entropy('Correct Horse Battery Staple'))->toBeGreaterThan(100);
+    expect(Strings::entropy('evil-chuck-666'))->toBeGreaterThan(40);
+    expect(Strings::entropy('acegik'))->toBeLessThan(15);
+    expect(Strings::entropy('12345'))->toBeLessThan(10);
+    expect(Strings::entropy('1'))->toBe(0.0);
+});
