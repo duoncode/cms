48b7880808df45650a885bad9382f4b050d9f1d6
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-04-08 20:09:41 +0200

    Define media queries

--- gui/postcss.config.cjs
+++ gui/postcss.config.cjs
@@ -1,7 +1,15 @@
+const postcssImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
+const postcssCustomMedia = require('postcss-custom-media');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-nested': {},
-        autoprefixer: {},
-    },
+    plugins: [
+        postcssImport(),
+        postcssNested(),
+        postcssCustomMedia({
+            importFrom: 'src/styles/vars.css',
+        }),
+        autoprefixer(),
+    ],
 };
