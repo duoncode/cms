c9c383e6598424b86aa1f943e1f246342dc3d207
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-04-08 15:22:22 +0200

    No Typescript for now

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
     server: {
         port: 2009,
         strictPort: true,
         proxy: {
-            '/conia':  'http://localhost:1983',
+            '/conia': 'http://localhost:1983',
         },
     },
 });
