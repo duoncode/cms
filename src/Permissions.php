3923d41b12561ebde8b65d7f5252969ec74ef091
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-04-11 17:06:09 +0200

    Initialize Permission with config

--- src/Permissions.php
+++ src/Permissions.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Conia;
 
+use Conia\Config;
+
 
 class Permissions
 {
@@ -34,13 +36,22 @@ class Permissions
         ],
     ];
 
+    public function __construct(protected Config $config)
+    {
+    }
+
     public function addPermission(string $role, string $permission)
     {
         $this->permissions[$role][] = $permission;
     }
 
-    public function hasPermission(string $role, string $permission)
+    public function hasPermission(string $role, string $permission): bool
     {
         return in_array($permission, $this->permissions[$role] ?? []);
+    }
+
+    public function get(string $role): array
+    {
+        return $this->permissions[$role] ?? [];
     }
 }
