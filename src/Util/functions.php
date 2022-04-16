4694cdf6ec3a57b7b3c0e215b4d1366ba387d650
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-04-16 11:23:15 +0200

    Add helper functions module with env function

---
+++ src/Util/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+
+if (!function_exists('env')) {
+    function env(string $key, bool|string|null $default = null): mixed
+    {
+        if (func_num_args() > 1) {
+            $value = $_ENV[$key] ?? null;
+
+            if ($value === null) {
+                return $default;
+            }
+        } else {
+            $value = $_ENV[$key];
+        }
+
+        return match ($value) {
+            'true' => true,
+            'false' => false,
+            'null' => null,
+            'empty' => '',
+            default => $value,
+        };
+    }
+}
