2df5d6b58f2d491d2d7ee9608e261daa092d68ea
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-04-21 18:25:26 +0200

    Add Category and Section

---
+++ src/Section.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia;
+
+
+class Section
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $title,
+        public readonly array $categories,
+    ) {
+    }
+}
