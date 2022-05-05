a5306a569c4fa70f025ad3c2b155669ded7523bf
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-05-05 21:25:36 +0200

    Extract NavLogo

--- gui/src/shell/Nav.svelte
+++ gui/src/shell/Nav.svelte
@@ -5,6 +5,7 @@
     import { navVisible } from '../lib/ui';
     import Backdrop from './Backdrop.svelte';
     import NavClose from './NavClose.svelte';
+    import NavLogo from './NavLogo.svelte';
 </script>
 
 <style type="postcss">
@@ -29,26 +30,6 @@
         }
     }
 
-    .logo {
-        display: flex;
-        align-items: center;
-        margin-top: 19px;
-
-        img {
-            height: var(--s-6);
-            margin-left: -10px;
-        }
-
-        span {
-            display: inline-block;
-            margin-top: -1px;
-            margin-left: var(--s-3);
-            font-size: var(--s-5);
-            font-weight: 300;
-            letter-spacing: var(--s-3);
-        }
-    }
-
     @media (--lg) {
         #nav {
             position: relative;
@@ -60,10 +41,7 @@
 <Backdrop />
 <div id="nav" class:open={$navVisible} class:close={!$navVisible}>
     <NavClose />
-    <div class="logo">
-        <img src="logo.svg" alt="Logo" />
-        <span>CONIA</span>
-    </div>
+    <NavLogo />
     {#each $system.sections as section}
         <h2>{section.title}</h2>
     {/each}
---
+++ gui/src/shell/NavLogo.svelte
@@ -0,0 +1,26 @@
+<style type="postcss">
+    .logo {
+        display: flex;
+        align-items: center;
+        margin-top: 19px;
+
+        img {
+            height: var(--s-6);
+            margin-left: -10px;
+        }
+
+        span {
+            display: inline-block;
+            margin-top: -1px;
+            margin-left: var(--s-3);
+            font-size: var(--s-5);
+            font-weight: 300;
+            letter-spacing: var(--s-3);
+        }
+    }
+</style>
+
+<div class="logo">
+    <img src="logo.svg" alt="Logo" />
+    <span>CONIA</span>
+</div>
