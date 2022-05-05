501587b8acc742c99260b4371febda42f8856859
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-05-05 18:16:40 +0200

    Save toggle state of nav in localStorage

--- gui/src/lib/ui.js
+++ gui/src/lib/ui.js
@@ -1,17 +1,35 @@
 import { writable } from 'svelte/store';
 
-const navVisible = writable(true);
+let initNavVisibility = true;
+
+if (localStorage.getItem('navVisible') === 'false') {
+    initNavVisibility = false;
+}
+
+const navVisible = writable(initNavVisibility);
 
 function closeNav() {
     navVisible.set(false);
+    localStorage.setItem('navVisible', 'false');
 }
 
 function openNav() {
     navVisible.set(true);
+    localStorage.setItem('navVisible', 'true');
 }
 
 function toggleNav() {
-    navVisible.update(state => !state);
+    navVisible.update(state => {
+        let newState = !state;
+
+        if (newState) {
+            localStorage.setItem('navVisible', 'true');
+        } else {
+            localStorage.setItem('navVisible', 'false');
+        }
+
+        return newState;
+    });
 }
 
 export {
