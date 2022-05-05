bb73802e83357461618914c1f3e04461dc8718e9
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-05-05 18:07:18 +0200

    Move toggle button out of toolbar, add searchbar

---
+++ gui/src/shell/NavToggle.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { toggleNav } from '../lib/ui';
+</script>
+
+<style>
+    button.nav {
+        padding: var(--sz-2);
+        margin-left: var(--sz-2);
+
+        background-color: var(--white);
+        border: 0;
+        box-shadow: none;
+        cursor: pointer;
+    }
+</style>
+
+<button class="nav" on:click={toggleNav}>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor">
+        <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd" />
+    </svg>
+</button>
