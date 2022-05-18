a7d44ea927b8ef70b25765f913adf194925a5bba
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-05-18 21:57:12 +0200

    Add value and default to Field

--- src/Field/Field.php
+++ src/Field/Field.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Conia\Field;
 
 use \Exception;
+use \RuntimeException;
 use Conia\Data;
 
 
 abstract class Field implements Data
 {
     public readonly string $type;
+    public string $defaultl;
+    public string $value;
 
     public function __construct(
         protected string $label,
@@ -40,5 +43,33 @@ abstract class Field implements Data
             'multilang' => $this->multilang,
             'description' => $this->description,
         ];
+    }
+
+    public function __toString(): string
+    {
+        return $this->getValue();
+    }
+
+    public function getValue(): string
+    {
+        if (isset($this->value)) {
+            return $this->value;
+        }
+
+        if (isset($this->default)) {
+            return $this->default;
+        }
+
+        throw new RuntimeException('No value available');
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->default = $value;
+    }
+
+    public function setDefault(string|array $value): void
+    {
+        $this->default = $value;
     }
 }
