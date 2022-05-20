dec98066539caf55e642ffc1d9e1fdd85d9fdbc3
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-05-20 08:49:07 +0200

    Add api ideas to Type

--- src/Type.php
+++ src/Type.php
@@ -5,23 +5,40 @@ declare(strict_types=1);
 namespace Conia;
 
 use \Generator;
+use \ValueError;
 use Conia\Data;
 
 
 abstract class Type
 {
-    public readonly string $name;
-    public readonly string $template;
+    protected static $name = null;
 
     public function __construct(
         public readonly string|array $label,
         public readonly array $permissions = [],
-        ?string $name = null,
-        ?string $template = null,
+        public readonly int $columns = 12,
     ) {
-        $class = basename(str_replace('\\', '/', strtolower($this::class)));
-        $this->name = $name ?: $class;
-        $this->template = $template ?:  $class . '.php';
+        if ($columns < 12 || $columns > 25) {
+            throw new ValueError('The value of $columns must be >= 12 and <= 25');
+        }
+    }
+
+    public static function fromUid(string $uid): self
+    {
+    }
+
+    public static function fromSlug(string $uid): self
+    {
+    }
+
+    public function name(): string
+    {
+        return static::$name ?: basename(str_replace('\\', '/', strtolower($this::class)));
+    }
+
+    public function template(): string
+    {
+        return $this->name() . '.php';
     }
 
     public function get(): Generator
@@ -45,5 +62,9 @@ abstract class Type
         }
 
         return $result;
+    }
+
+    public function renameField(string $current, string $new): void
+    {
     }
 }
