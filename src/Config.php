d19f872491f0fb0dad20998a7da9b56f15e7cf89
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-06-21 18:52:26 +0200

    Update config to new format

--- src/Config.php
+++ src/Config.php
@@ -5,30 +5,50 @@ declare(strict_types=1);
 namespace Conia;
 
 use \Exception;
+use \ValueError;
 use Chuck\Config as BaseConfig;
+use Chuck\Config\Connection;
 
 
 class Config extends BaseConfig
 {
-    const TYPES = 'types';
+    protected string $panelPath = 'panel';
+    protected string $theme = null;
+    protected array $tpyes = [];
 
-    public function __construct(array $config)
+    public function __construct(
+        string $app,
+        string $dsn,
+        bool $debug = false,
+        string $env = ''
+    ) {
+        parent::__construct($app, $debug, $env);
+        $root = dirname(__DIR__);
+
+        $this->addConnection(new Connection(
+            $dsn,
+            $root . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'sql',
+            $root . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'migrations',
+        ), 'conia');
+
+        $this->set('panel.path', 'panel');
+        $this->set('panel.theme', null);
+        $this->set('session.expires', 60 * 60 * 24);
+        $this->set('session.authcookie', $app . '_auth');
+
+        $this->scripts()->add($root . DIRECTORY_SEPARATOR . 'bin');
+    }
+
+    public function setPanelPath(string $url): void
     {
-        $coniaConfig = [
-            'panel.path' => 'panel',
-            'panel.theme' => null,
-            'sql.conia' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'sql',
-            'migrations.conia' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'migrations',
-            'scripts.conia' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin',
-            'session.expires' => 60 * 60 * 24,
-            'session.authcookie' => $config['app'] . '_auth',
-        ];
-        $coniaConfig[self::TYPES] = [];
-
-        parent::__construct(array_replace_recursive(
-            $coniaConfig,
-            $config,
-        ));
+        if (preg_match('/^[A-Za-z0-9]{1,32}$/', $url)) {
+            $this->panelPath = $url;
+        } else {
+            throw new ValueError(
+                'The panel url prefix be a nonempty string which consist only of letters' .
+                    ' and numbers. Its length must not be longer than 32 characters.'
+            );
+        }
     }
 
     public function addType(Type $type): void
