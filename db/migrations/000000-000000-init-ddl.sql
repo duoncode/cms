a95f5e352ab7d32c5dc5a0f258a769bf6cfe5411
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-06-29 19:41:17 +0200

    Add name column to pagetypes table

--- db/migrations/000000-000000-init-ddl.sql
+++ db/migrations/000000-000000-init-ddl.sql
@@ -88,9 +88,10 @@ CREATE TABLE conia.loginsessions (
 
 CREATE TABLE conia.pagetypes (
     pagetype integer GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL CHECK (char_length(classname) <= 64),
     classname text NOT NULL CHECK (char_length(classname) <= 256),
     CONSTRAINT pk_pagetypes PRIMARY KEY (pagetype),
-    CONSTRAINT uc_pagestypes_classname UNIQUE (classname)
+    CONSTRAINT uc_pagestypes_name UNIQUE (name)
 );
 
 
