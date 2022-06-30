ec69273c6c6b4cd30e55f9bead96e5f70ee3b901
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-06-30 17:03:11 +0200

    Add Value\Time

---
+++ src/Value/Time.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Value;
+
+use \IntlDateFormatter;
+
+
+class Time extends DateTime
+{
+    const FORMAT = 'H:i:s';
+
+
+    public function localize(
+        ?string $locale = null,
+        int $dateFormat = IntlDateFormatter::NONE,
+        int $timeFormat = IntlDateFormatter::SHORT,
+    ): string {
+        return parent::localize($locale, $dateFormat, $timeFormat);
+    }
+}
