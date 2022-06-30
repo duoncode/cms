8484d59947121ccdca8270a15ff66e6bde63ba38
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-06-30 16:57:12 +0200

    Add Value\{Date, DateTime}

---
+++ src/Value/Date.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Value;
+
+use \IntlDateFormatter;
+
+
+class Date extends DateTime
+{
+    const FORMAT = 'Y-m-d';
+
+
+    public function localize(
+        ?string $locale = null,
+        int $dateFormat = IntlDateFormatter::MEDIUM,
+        int $timeFormat = IntlDateFormatter::NONE,
+    ): string {
+        return parent::localize($locale, $dateFormat, $timeFormat);
+    }
+}
---
+++ src/Value/DateTime.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Value;
+
+use \DateTimeImmutable;
+use \DateTimeZone;
+use \IntlDateFormatter;
+use Conia\Value;
+
+
+class DateTime extends Value
+{
+    const FORMAT = 'Y-m-d H:i:s';
+
+    public readonly ?DateTimeImmutable $datetime;
+    public readonly ?DateTimeZone $timezone;
+
+    public function __construct(
+        array $data,
+        string $locale
+    ) {
+        parent::__construct($data, $locale);
+
+        if ($data['timezone'] ?? null) {
+            $this->timezone = new DateTimeZone($data['timezone']);
+        } else {
+            $this->timezone = null;
+        }
+
+        if ($data['value'] ?? null) {
+            $this->datetime = DateTimeImmutable::createFromFormat(
+                static::FORMAT,
+                $data['value'],
+                $this->timezone,
+            );
+        } else {
+            $this->datetime = null;
+        }
+    }
+
+    public function format(string $format): string
+    {
+        if ($this->datetime) {
+            return $this->datetime->format($format);
+        }
+
+        return '';
+    }
+
+    public function localize(
+        ?string $locale = null,
+        int $dateFormat = IntlDateFormatter::MEDIUM,
+        int $timeFormat = IntlDateFormatter::MEDIUM,
+    ): string {
+        if ($this->datetime) {
+            $formatter = new IntlDateFormatter(
+                $locale ?: $this->locale,
+                $dateFormat,
+                $timeFormat,
+                $this->timezone
+            );
+
+            return $formatter->format($this->datetime->getTimestamp());
+        }
+
+        return '';
+    }
+
+    public function __toString(): string
+    {
+        return $this->format(static::FORMAT);
+    }
+}
