4d903af1ffd19d41913fa42cdeedd811f4057b9b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-02 11:40:24 +0200

    Add i18n settings to Matrx field

--- src/Field/Matrix.php
+++ src/Field/Matrix.php
@@ -12,7 +12,11 @@ use Conia\Value\{Value, Matrix as MatrixValue};
 
 class Matrix extends Field
 {
-    public int $columns = 12;
+    const I18N_MIXED = 'mixed';
+    const I18N_SEPARATE = 'separate';
+
+    protected int $columns = 12;
+    protected string $i18n = 'mixed';
 
     public function __toString(): string
     {
@@ -28,6 +32,27 @@ class Matrix extends Field
         $this->columns = $columns;
 
         return $this;
+    }
+
+    public function getColumns(): int
+    {
+        return $this->columns;
+    }
+
+    public function i18n(string $i18n): static
+    {
+        if ($i18n === self::I18N_MIXED || $i18n === self::I18N_SEPARATE) {
+            $this->i18n = $i18n;
+
+            return $this;
+        }
+
+        throw new ValueError('Wrong i18n value. Use the Matrix class constants');
+    }
+
+    public function getI18N(): string
+    {
+        return $this->i18n;
     }
 
     public function value(Request $request, array $data): Value
