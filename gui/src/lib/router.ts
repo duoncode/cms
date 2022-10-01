d0bbcad4a5c28b6224f70de7c08a67737c220ae5
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-10-01 17:32:04 +0200

    Type gui/lib/router.ts

--- gui/src/lib/router.ts
+++ gui/src/lib/router.ts
@@ -1,24 +1,30 @@
+import type { SvelteComponent } from 'svelte';
+import type { WrappedComponent } from 'svelte-spa-router';
 import { wrap } from 'svelte-spa-router/wrap';
 
+type Component = SvelteComponent | WrappedComponent;
+type RouteMap = Map<string, Component>;
 
 export default class Router {
+    protected routes: RouteMap;
+
     constructor() {
         this.routes = new Map();
     }
 
-    settings(options) {
-        if (options.component || options.asyncComponent) {
+    settings(options: Component) {
+        if ('component' in options || 'asyncComponent' in options) {
             return wrap(options);
         }
 
         return options;
     }
 
-    add(pattern, options) {
+    add(pattern: string, options: SvelteComponent) {
         this.routes.set(pattern, this.settings(options));
     }
 
-    get() {
+    get(): RouteMap {
         return this.routes;
     }
 }
