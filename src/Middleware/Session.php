3fb6a08f18f1f99ec22ac44d210496e332698949
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-21 15:14:20 +0100

    Add session as request attribute in middleware

--- src/Middleware/Session.php
+++ src/Middleware/Session.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Conia\Middleware;
 
-use Conia\Chuck\RequestInterface;
-use Conia\Chuck\Response\ResponseInterface;
-
+use Conia\Chuck\Request;
+use Conia\Chuck\Response;
+use Conia\Config;
 
 class Session
 {
-    public function __invoke(RequestInterface $request, callable $next): RequestInterface|ResponseInterface
+    public function __construct(protected Config $config)
     {
-        $config = $request->config();
-        $session = new \Conia\Session($config->app(), $config->get('session.authcookie'));
+    }
+
+    public function __invoke(Request $request, callable $next): Response
+    {
+        $session = new \Conia\Session\Session(
+            $this->config->app(),
+            $this->config->get('sessionOptions')
+        );
         $session->start();
 
-        $request->addMethod('session', function () use ($session): \Conia\Session {
-            return $session;
-        });
+        $request->attribute('session', $session);
 
         return $next($request);
     }
