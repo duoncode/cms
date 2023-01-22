defde7bda0cb8f3f0af4ed715d941061ecd48ac1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-22 19:09:47 +0100

    Use Symfony HTML sanitizer directly in Html value

--- src/Value/Html.php
+++ src/Value/Html.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Conia\Core\Value;
 
-use Conia\Chuck\Util\Html as Sanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
 use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
 
 class Html extends Text
@@ -18,6 +18,16 @@ class Html extends Text
         ?HtmlSanitizerConfig $config = null,
         bool $removeEmptyLines = true,
     ): string {
-        return Sanitizer::clean($this->raw(), $config, $removeEmptyLines);
+        $config = $config ?: (new HtmlSanitizerConfig())
+            // Allow "safe" elements and attributes. All scripts will be removed
+            // as well as other dangerous behaviors like CSS injection
+            ->allowSafeElements();
+        $sanitizer = new HtmlSanitizer($config);
+        $result = $sanitizer->sanitize($this->raw());
+
+        // also remove empty lines
+        return $removeEmptyLines ?
+            preg_replace("/(^[\r\n]*|[\r\n]+)[\\s\t]*[\r\n]+/", PHP_EOL, $result) :
+            $result;
     }
 }
