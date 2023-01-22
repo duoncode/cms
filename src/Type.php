42102494125f8b1893fe8bc88ff9b0f04f8c5065
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-22 19:17:24 +0100

    Add support of Type field/property attributes

--- src/Type.php
+++ src/Type.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Conia\Core;
 
+use Conia\Chuck\Request;
+use Conia\Core\Exception\NoSuchField;
+use Conia\Core\Exception\ValueError;
+use Conia\Core\Field\Attr\Description;
+use Conia\Core\Field\Attr\Height;
+use Conia\Core\Field\Attr\Label;
+use Conia\Core\Field\Attr\MultiLang;
+use Conia\Core\Field\Attr\Required;
+use Conia\Core\Field\Attr\Width;
 use Conia\Core\Field\Field;
 use Conia\Core\Value\Value;
 use Generator;
-use RuntimeException;
-use ValueError;
+use ReflectionClass;
+use ReflectionProperty;
 
 abstract class Type
 {
@@ -18,36 +27,32 @@ abstract class Type
     protected static int $columns = 12;
 
     protected array $list = [];
-    protected array $fields = [];
 
     final public function __construct(
         protected readonly Request $request,
         protected readonly array $data,
     ) {
-        $this->init();
+        $this->initFields();
     }
 
-    final public function __get(string $name): Value
+    final public function __get(string $field): Value
     {
-        if (!array_key_exists($name, $this->fields)) {
+        if (!isset($this->{$field})) {
             $type = $this::class;
 
-            throw new RuntimeException("Type {$type} has no field '{$name}'.");
+            throw new NoSuchField("The field '{$field}' does not exist on Type '{$type}'.");
         }
 
-        $field = $this->fields[$name];
-        $content = $this->data['content'][$name] ?? [];
+        $content = $this->data['content'][$field] ?? [];
+        $field = $this->{$field};
 
         return $field->value($this->request, $content);
     }
 
-    final public function __set(string $name, Field $field): void
+    public function init(): void
     {
-        $this->list[] = $name;
-        $this->fields[$name] = $field;
+        // can be overwritten
     }
-
-    abstract public function init(): void;
 
     abstract public function title(): string;
 
@@ -106,6 +111,73 @@ abstract class Type
         ];
 
         return array_merge($data, $content);
+    }
+
+    protected function initFields(): void
+    {
+        $this->init();
+
+        $rc = new ReflectionClass(static::class);
+
+        foreach ($rc->getProperties() as $property) {
+            $name = $property->getName();
+
+            if (!$property->hasType()) {
+                continue;
+            }
+
+            $fieldType = $property->getType()->getName();
+
+            if (is_subclass_of($fieldType, Field::class)) {
+                if (isset($this->{$name})) {
+                    continue;
+                }
+
+                $this->{$name} = $this->initField($property, $fieldType);
+            }
+        }
+    }
+
+    protected function initField(ReflectionProperty $property, string $fieldType): Field
+    {
+        $field = new $fieldType($property->getName());
+
+        foreach ($property->getAttributes() as $attr) {
+            switch ($attr->getName()) {
+                case Required::class:
+                    $field->required(true);
+
+                    break;
+
+                case MultiLang::class:
+                    $field->multilang(true);
+
+                    break;
+
+
+                case Label::class:
+                    $field->label($attr->newInstance()->label);
+
+                    break;
+
+                case Description::class:
+                    $field->description($attr->newInstance()->description);
+
+                    break;
+
+                case Width::class:
+                    $field->width($attr->newInstance()->width);
+
+                    break;
+
+                case Height::class:
+                    $field->height($attr->newInstance()->height);
+
+                    break;
+            }
+        }
+
+        return $field;
     }
 
     protected function getJsonContent(): array
