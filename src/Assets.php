917061c748867ec76e70decb5e01aab570596300
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-23 09:22:04 +0100

    Add Assets

---
+++ src/Assets.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Core;
+
+class Assets extends \Conia\Sizer\Assets
+{
+    public static function fromConfig(Config $config): \Conia\Sizer\Assets
+    {
+        $public = $config->get('path.public');
+
+        return new \Conia\Sizer\Assets(
+            $public . '/' . $config->get('path.assets'),
+            $public . '/' . $config->get('path.cache'),
+        );
+    }
+}
