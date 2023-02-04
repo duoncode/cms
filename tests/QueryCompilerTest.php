b2a4e0acb5ad8529f51675eb91bdde0c72970ba5
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-02-04 10:39:00 +0100

    Update compiler tests

--- tests/QueryCompilerTest.php
+++ tests/QueryCompilerTest.php
@@ -36,7 +36,7 @@ test('Json string quoting', function () {
     );
 });
 
-test('Compile field with number value', function () {
+test('Number operand', function () {
     $compiler = new QueryCompiler($this->context, []);
 
     expect($compiler->compile('field = 13'))->toBe("p.content @@ '$.field.value == 13'");
@@ -45,7 +45,7 @@ test('Compile field with number value', function () {
     expect($compiler->compile('field.value.de = 13.73'))->toBe("p.content @@ '$.field.value.de == 13.73'");
 });
 
-test('Compile field with string value', function () {
+test('String operand', function () {
     $compiler = new QueryCompiler($this->context, []);
 
     expect($compiler->compile('field = "string"'))->toBe('p.content @@ \'$.field.value == "string"\'');
@@ -56,7 +56,7 @@ test('Compile field with string value', function () {
     expect($compiler->compile('field.value.de = /string/'))->toBe('p.content @@ \'$.field.value.de == "string"\'');
 });
 
-test('Compile field with boolean value', function () {
+test('Boolean operand', function () {
     $compiler = new QueryCompiler($this->context, []);
 
     expect($compiler->compile('field = false'))->toBe("p.content @@ '$.field.value == false'");
@@ -65,7 +65,7 @@ test('Compile field with boolean value', function () {
     expect($compiler->compile('field.value.de = true'))->toBe("p.content @@ '$.field.value.de == true'");
 });
 
-test('Compile regex comparison', function () {
+test('Operator regex operand pattern', function () {
     $compiler = new QueryCompiler($this->context, []);
 
     expect($compiler->compile('field ~ /^test$/'))->toBe("p.content @? '$.field.value ? (@ like_regex \"^test$\")'");
@@ -75,19 +75,19 @@ test('Compile regex comparison', function () {
     expect($compiler->compile('field !~* /^test$/'))->toBe("NOT p.content @? '$.field.value ? (@ like_regex \"^test$\" flag \"i\")'");
 });
 
-test('Compile multilang field', function () {
+test('Multilang field operand', function () {
     $compiler = new QueryCompiler($this->context, []);
 
     expect($compiler->compile('field.* = "test"'))->toBe("p.content @@ '$.field.value.* == \"test\"'");
 });
 
-test('Compile builtin', function () {
+test('Builtin operand', function () {
     $compiler = new QueryCompiler($this->context, ['test' => 'table.test']);
 
     expect($compiler->compile('test = 1'))->toBe('table.test = 1');
 });
 
-test('Compile keyword now', function () {
+test('Keyword now', function () {
     $compiler = new QueryCompiler($this->context, ['test' => 'test']);
 
     expect($compiler->compile('test = now'))->toBe('test = NOW()');
