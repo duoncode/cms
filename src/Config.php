759f845024e8546b7610a88ea6e2660f62c984cf
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-04-05 12:53:15 +0200

    Move Chuck config logic here

--- src/Config.php
+++ src/Config.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Conia\Core;
 
 use Closure;
-use Conia\Chuck\Config as BaseConfig;
+use Conia\Chuck\Exception\OutOfBoundsException;
+use Conia\Chuck\Exception\ValueError;
 use Conia\Core\Locales;
+use Throwable;
 
-class Config extends BaseConfig
+class Config
 {
     public readonly Locales $locales;
     public readonly Nodes $nodes;
@@ -16,15 +18,16 @@ class Config extends BaseConfig
     protected ?string $panelTheme = null;
     protected Closure $languageNegotiator;
     private bool $debugPanel = false;
+    private array $settings;
 
     public function __construct(
         public readonly string $root,
-        string $app = 'conia',
-        bool $debug = false,
-        string $env = '',
+        public readonly string $app = 'conia',
+        public readonly bool $debug = false,
+        public readonly string $env = '',
         array $settings = [],
     ) {
-        $settings = array_merge([
+        $this->settings = array_merge([
             'path.public' => "{$root}/public",
             'path.assets' => '/assets',
             'path.cache' => '/cache',
@@ -33,10 +36,49 @@ class Config extends BaseConfig
             // 'password.entropy' => float
         ], $settings);
 
-        parent::__construct($app, $debug, $env, $settings);
-
+        $this->validateApp($app);
         $this->locales = new Locales();
         $this->nodes = new Nodes();
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->settings[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->settings);
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        if (isset($this->settings[$key])) {
+            return $this->settings[$key];
+        }
+
+        if (func_num_args() > 1) {
+            return $default;
+        }
+
+        throw new OutOfBoundsException(
+            "The configuration key '{$key}' does not exist"
+        );
+    }
+
+    public function app(): string
+    {
+        return $this->app;
+    }
+
+    public function debug(): bool
+    {
+        return $this->debug;
+    }
+
+    public function env(): string
+    {
+        return $this->env;
     }
 
     public function debugPanel(bool $debug = true): bool
@@ -103,5 +145,15 @@ class Config extends BaseConfig
     public function locales(): Locales
     {
         return $this->locales;
+    }
+
+    protected function validateApp(string $app): void
+    {
+        if (!preg_match('/^[a-zA-Z0-9_$-]{1,64}$/', $app)) {
+            throw new ValueError(
+                'The app name must be a nonempty string which consist only of lower case ' .
+                    'letters and numbers. Its length must not be longer than 32 characters.'
+            );
+        }
     }
 }
