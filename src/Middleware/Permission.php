cbde56c3f715ff3145e0af29981a7d9c39854776
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-09 14:00:01 +0200

    Add init method to Permission middleware

--- src/Middleware/Permission.php
+++ src/Middleware/Permission.php
@@ -4,27 +4,34 @@ declare(strict_types=1);
 
 namespace Conia\Core\Middleware;
 
-use Conia\Chuck\Error\HttpForbidden;
-use Conia\Chuck\Error\HttpUnauthorized;
+use Conia\Chuck\Di\Call;
+use Conia\Chuck\Exception\HttpForbidden;
+use Conia\Chuck\Exception\HttpUnauthorized;
 use Conia\Chuck\Middleware;
 use Conia\Chuck\Request;
 use Conia\Chuck\Response;
 use Conia\Core\Auth;
+use Conia\Core\Config;
 use Conia\Core\Permissions;
+use Conia\Core\Users;
 
+#[Call('init')]
 class Permission implements Middleware
 {
+    protected Users $users;
+    protected Config $config;
+
     public function __construct(protected string $permission)
     {
     }
 
     public function __invoke(Request $request, callable $next): Response
     {
-        $auth = new Auth($request);
+        $auth = new Auth($request, $this->users, $this->config, $request->get('session'));
         $user = $auth->user();
 
         if ($user) {
-            $permission = new Permissions($request->config());
+            $permission = new Permissions();
 
             if (!$permission->has($user['role'], $this->permission)) {
                 throw new HttpForbidden();
@@ -34,5 +41,11 @@ class Permission implements Middleware
         }
 
         throw new HttpUnauthorized();
+    }
+
+    public function init(Users $users, Config $config): void
+    {
+        $this->users = $users;
+        $this->config = $config;
     }
 }
