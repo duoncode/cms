c3f916eef09dfa79349ecdd1d49da1895d04bdf1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-14 22:37:01 +0200

    List pages in collection component

--- gui/src/routes/(panel)/collections/[collection]/+page.svelte
+++ gui/src/routes/(panel)/collections/[collection]/+page.svelte
@@ -6,3 +6,9 @@
 
 <Searchbar />
 <h1>Pages {data.collection}</h1>
+
+<div class="mt-4">
+    {#each data.pages as page}
+        <div class="border-bottom">{page.title}, {page.type}</div>
+    {/each}
+</div>
