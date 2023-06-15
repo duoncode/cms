5bc32790d6992441eb3fb59798bd3e488fe7e9ba
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-15 19:36:29 +0200

    Implement Option::asArray

--- src/Field/Option.php
+++ src/Field/Option.php
@@ -10,14 +10,33 @@ use Conia\Core\Value;
 class Option extends Field
 {
     protected array $options = [];
+    protected bool $hasLabel = false;
 
     public function value(): Value\Option
     {
         return new Value\Option($this->node, $this, $this->valueContext);
     }
 
-    public function add(string|array $option)
+    public function add(string|array $option): void
     {
         $this->options[] = $option;
+    }
+
+    public function options(array $options): void
+    {
+        if (is_array($options[0])) {
+            $this->hasLabel = true;
+        }
+
+        $this->options = $options;
+    }
+
+    public function asArray(): array
+    {
+        $result = parent::asArray();
+        $result['options'] = $this->options;
+        $result['hasLabel'] = $this->hasLabel;
+
+        return $result;
     }
 }
