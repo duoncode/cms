b2bc9a7662543dab8bfb888beb2a1a2dcffaf866
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-16 12:27:35 +0200

    Extract icons into components

--- gui/src/routes/(panel)/collection/[collection]/[node]/+page.svelte
+++ gui/src/routes/(panel)/collection/[collection]/[node]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import NodeControlBar from '$shell/NodeControlBar.svelte';
+    import ChevronRight from '$shell/icons/ChevronRight.svelte';
+    import DocumentTree from '$shell/icons/DocumentTree.svelte';
     import controls from '$lib/controls';
 
     export let data;
@@ -21,7 +23,7 @@
         grid-template-columns: repeat(100, minmax(0, 1fr));
     }
 
-    svg {
+    .breadcrumbs :global(svg) {
         color: #999;
         display: inline-block;
     }
@@ -29,26 +31,10 @@
 
 <NodeControlBar uid={data.node.uid} title={data.node.title} />
 
-<div class="max-w-7xl mx-auto mt-8 flex flex-row items-center gap-3">
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="1em"
-        viewBox="0 0 576 512"
-        fill="currentColor"
-        ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-            d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32v96V384c0 35.3 28.7 64 64 64H256V384H64V160H256V96H64V32zM288 192c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4L409.4 9.4c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V192zm0 288c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V352c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4l-13.3-13.3c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V480z" /></svg>
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-5 h-5">
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-    </svg>
+<div
+    class="breadcrumbs max-w-7xl mx-auto mt-8 flex flex-row items-center gap-3">
+    <DocumentTree />
+    <ChevronRight />
     <span>
         <a
             href="/panel/collection/{data.collection.slug}"
@@ -56,18 +42,7 @@
             {data.collection.title}
         </a>
     </span>
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-5 h-5">
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-    </svg>
+    <ChevronRight />
     <span class="font-medium">{data.node.title}</span>
 </div>
 
--- gui/src/shell/NavToggle.svelte
+++ gui/src/shell/NavToggle.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { toggleNav } from '../lib/ui';
+    import BarsCenterLeft from '$shell/BarsCenterLeft.svelte';
 </script>
 
 <style lang="postcss">
@@ -14,13 +15,5 @@
 </style>
 
 <button class="nav" on:click={toggleNav}>
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor">
-        <path
-            fill-rule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clip-rule="evenodd" />
-    </svg>
+    <BarsCenterLeft />
 </button>
