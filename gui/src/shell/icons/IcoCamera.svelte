4cd8bc4de4b93813dcaa551713fb497c168dce1f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-19 22:51:30 +0200

    Better camera and slashed circle icons

--- gui/src/shell/icons/IcoCamera.svelte
+++ gui/src/shell/icons/IcoCamera.svelte
@@ -1,11 +1,6 @@
 <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
     fill="currentColor"
-    class="bi bi-camera2"
-    viewBox="0 0 16 16">
-    <path d="M5 8c0-1.657 2.343-3 4-3V4a4 4 0 0 0-4 4z" />
-    <path
-        d="M12.318 3h2.015C15.253 3 16 3.746 16 4.667v6.666c0 .92-.746 1.667-1.667 1.667h-2.015A5.97 5.97 0 0 1 9 14a5.972 5.972 0 0 1-3.318-1H1.667C.747 13 0 12.254 0 11.333V4.667C0 3.747.746 3 1.667 3H2a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1h.682A5.97 5.97 0 0 1 9 2c1.227 0 2.367.368 3.318 1zM2 4.5a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0zM14 8A5 5 0 1 0 4 8a5 5 0 0 0 10 0z" />
-</svg>
+    viewBox="0 0 512 512"
+    ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+        d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" /></svg>
