2d48e75675ca1076dd1fa8442a4ce202212a0d7d
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-19 19:24:26 +0200

    Add Hidden, Date, Time and Number components

---
+++ gui/src/shell/controls/Time.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import Field from '$shell/Field.svelte';
+    import Label from '$shell/Label.svelte';
+    import type { TextData } from '$types/data';
+    import type { SimpleField } from '$types/fields';
+
+    export let field: SimpleField;
+    export let data: TextData;
+</script>
+
+<Field required={field.required}>
+    <Label of={field.name}>
+        {field.label}
+    </Label>
+    <div class="mt-2">
+        <input
+            id={field.name}
+            name={field.name}
+            type="time"
+            required={field.required}
+            bind:value={data.value} />
+    </div>
+</Field>
