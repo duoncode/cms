c8cbf5b569cf83d71e504bb50fa9d6a233ac8bbd
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-20 06:39:49 +0200

    Add GridHtml component

--- gui/src/shell/controls/GridHtml.svelte
+++ gui/src/shell/controls/GridHtml.svelte
@@ -1,1 +1,17 @@
-HTML
+<script lang="ts">
+    import Wysiwyg from '$shell/Wysiwyg.svelte';
+    import type { GridImage } from '$types/data';
+    import type { GridField } from '$types/fields';
+
+    export let field: GridField;
+    export let data: GridImage;
+    export let node: string;
+    export let index: number;
+</script>
+
+<div class="col-span-{data.colspan} row-span-{data.rowspan}">
+    <Wysiwyg
+        required={false}
+        name={field.name + '_' + index}
+        bind:value={data.value} />
+</div>
