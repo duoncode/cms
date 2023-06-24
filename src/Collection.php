aeb638e6f8997b9d0ed2964fc8e2d7156b450715
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-24 21:17:40 +0200

    Add abstract method Collection::newEntryNodes

--- src/Collection.php
+++ src/Collection.php
@@ -14,6 +14,7 @@ abstract class Collection
     }
 
     abstract public function entries(): Nodes;
+    abstract public function newEntryNodes(): array;
 
     public function title(): string
     {
