46b387754d2abe4497b7b600cc148f15afc9d056
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-25 11:58:45 +0200

    Add Button component

---
+++ gui/src/shell/Button.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type { ComponentType } from 'svelte';
+    let cls = 'primary';
+
+    export let icon: ComponentType = null;
+    export { cls as class };
+</script>
+
+<style lang="postcss">
+    button {
+        @apply relative border font-medium rounded-md;
+        @apply py-2 px-3 text-left focus:outline-none transition;
+        @apply flex flex-row items-center;
+
+        span {
+            @apply inline-block w-5 h-5 mr-2;
+        }
+    }
+
+    .danger {
+        @apply text-white bg-rose-800 border-rose-800;
+    }
+
+    .primary {
+        @apply text-white bg-emerald-800 border-emerald-800;
+    }
+</style>
+
+<button class={cls} on:click on:mouseover on:mouseenter on:mouseleave on:focus>
+    {#if icon}
+        <span>
+            <svelte:component this={icon} />
+        </span>
+    {/if}
+    <slot />
+</button>
--- gui/src/shell/NodeControlBar.svelte
+++ gui/src/shell/NodeControlBar.svelte
@@ -1,46 +1,29 @@
 <script lang="ts">
     import { _ } from '$lib/locale';
-    import NavToggle from './NavToggle.svelte';
+    import NavToggle from '$shell/NavToggle.svelte';
+    import Button from '$shell/Button.svelte';
     import IcoTrash from '$shell/icons/IcoTrash.svelte';
     import IcoSave from '$shell/icons/IcoSave.svelte';
 
     export let uid: string;
+
+    function remove() {
+        console.log('remove' + uid);
+    }
+
+    function save() {
+        console.log('save' + uid);
+    }
 </script>
-
-<style lang="postcss">
-    button {
-        @apply relative border font-medium rounded-md;
-        @apply py-2 px-3 text-left focus:outline-none transition;
-        @apply flex flex-row items-center;
-
-        span {
-            @apply inline-block w-5 h-5 mr-2;
-        }
-    }
-
-    .danger {
-        @apply text-white bg-emerald-600 border-emerald-600;
-    }
-
-    .primary {
-        @apply text-white bg-emerald-800 border-emerald-800;
-    }
-</style>
 
 <div class="headerbar">
     <NavToggle />
     <div class="controls flex flex-row gap-4 justify-end px-4">
-        <button class="danger">
-            <span>
-                <IcoTrash />
-            </span>
+        <Button class="danger" icon={IcoTrash} on:click={remove}>
             Löschen
-        </button>
-        <button class="primary">
-            <span>
-                <IcoSave />
-            </span>
+        </Button>
+        <Button class="primary" icon={IcoSave} on:click={save}>
             Speichern
-        </button>
+        </Button>
     </div>
 </div>
