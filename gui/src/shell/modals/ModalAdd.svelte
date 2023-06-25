e2a394279bf493dc3051088de8bba2ab157d7f77
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-06-25 12:46:29 +0200

    Revise ModalAdd

--- gui/src/shell/modals/ModalAdd.svelte
+++ gui/src/shell/modals/ModalAdd.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { _ } from '$lib/locale';
-    import { pristine } from '$lib/state';
+    import Button from '$shell/Button.svelte';
 
     export let add: (index: number, before: boolean, type: string) => void;
     export let close: () => void;
@@ -27,19 +27,6 @@
 </script>
 
 <style lang="postcss">
-    button {
-        @apply relative border border-sky-600 font-medium rounded-md;
-        @apply m-2 ml-0 p-2 text-left focus:outline-none transition;
-    }
-
-    .danger {
-        @apply text-white bg-orange-700 border-orange-700;
-    }
-
-    .primary {
-        @apply text-white bg-sky-700 border-sky-700;
-    }
-
     h2 {
         @apply font-medium mb-4;
     }
@@ -48,32 +35,31 @@
 <div class="message">
     <h2>{_('Inhaltstyp hinzufügen')}</h2>
     <div class="body">
-        <div class="grid grid-cols-2">
+        <div class="grid grid-cols-2 gap-4 mb-8">
             {#if types.length > 0}
                 {#each types as t}
-                    <button
-                        class=""
-                        class:bg-sky-600={t.id === type}
-                        class:text-sky-600={t.id !== type}
-                        class:text-white={t.id === type}
+                    <Button
+                        class="border-sky-800 {t.id === type
+                            ? 'bg-sky-800 text-white'
+                            : 'text-sky-800 bg-white'}"
                         on:click={setType(t.id)}>
                         <span class="ml-2">
                             {t.label}
                         </span>
-                    </button>
+                    </Button>
                 {/each}
             {/if}
         </div>
     </div>
-    <div class="controls text-right">
-        <button class="danger" on:click={close}>{_('Abbrechen')} </button>
-        <button class="primary" on:click={addContent(true)} {disabled}>
+    <div class="flex flex-row justify-end gap-2">
+        <Button class="danger" on:click={close}>{_('Abbrechen')}</Button>
+        <Button class="primary" on:click={addContent(true)} {disabled}>
             {index === null ? _('Einfügen') : _('Davor einfügen')}
-        </button>
+        </Button>
         {#if index !== null}
-            <button class="primary" on:click={addContent(false)} {disabled}>
+            <Button class="primary" on:click={addContent(false)} {disabled}>
                 {_('Danach einfügen')}
-            </button>
+            </Button>
         {/if}
     </div>
 </div>
