1f57684eff8e57e84254d6a3808125d921fcbf6f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-16 13:04:35 +0100

    Add namespace to util functions

--- src/Util/functions.php
+++ src/Util/functions.php
@@ -2,46 +2,42 @@
 
 declare(strict_types=1);
 
+namespace Conia\Core\Util;
+
 use Hidehalo\Nanoid\Client;
 
-if (!function_exists('env')) {
-    function env(string $key, bool|string|null $default = null): mixed
-    {
-        if (func_num_args() > 1) {
-            $value = $_ENV[$key] ?? null;
+function env(string $key, bool|string|null $default = null): mixed
+{
+    if (func_num_args() > 1) {
+        $value = $_ENV[$key] ?? null;
 
-            if ($value === null) {
-                return $default;
-            }
-        } else {
-            $value = $_ENV[$key];
+        if ($value === null) {
+            return $default;
         }
-
-        return match ($value) {
-            'true' => true,
-            'false' => false,
-            'null' => null,
-            'empty' => '',
-            default => $value,
-        };
+    } else {
+        $value = $_ENV[$key];
     }
+
+    return match ($value) {
+        'true' => true,
+        'false' => false,
+        'null' => null,
+        'empty' => '',
+        default => $value,
+    };
 }
 
-if (!function_exists('nanoid')) {
-    function nanoid()
-    {
-        $client = new Client();
+function nanoid()
+{
+    $client = new Client();
 
-        return $client->formattedId(
-            alphabet: '123456789bcdfghklmnpqrstvwxyz',
-            size: 13
-        );
-    }
+    return $client->formattedId(
+        alphabet: '123456789bcdfghklmnpqrstvwxyz',
+        size: 13
+    );
 }
 
-if (!function_exists('escape')) {
-    function escape(string $string)
-    {
-        return htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8');
-    }
+function escape(string $string)
+{
+    return htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8');
 }
