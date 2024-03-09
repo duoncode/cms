77459bf8c5a41282b7c25a9f1acd0a2ed6fce852
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-03-09 13:07:48 +0100

    Render video in Grid

--- src/Value/Video.php
+++ src/Value/Video.php
@@ -6,4 +6,10 @@ namespace Conia\Cms\Value;
 
 class Video extends File
 {
+    public function __toString(): string
+    {
+        $url   = $this->url();
+
+        return "<video controls><source src=\"{$url}\" type=\"video/mp4\"/></video>";
+    }
 }
