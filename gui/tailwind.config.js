80cd0c9f632aa32040ae017d83dc10124d9cff4b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-12-11 12:29:39 +0100

    Update infrastucture config

--- gui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-    safelist: [{ pattern: /^col-span-/ }, { pattern: /^row-span-/ }],
-};
