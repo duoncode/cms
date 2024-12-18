7d0c94f4ff5179be88127a4198ee862b08e4e703
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-12-18 20:41:48 +0100

    Add save button to embed view

--- gui/src/shell/EmbeddedNode.svelte
+++ gui/src/shell/EmbeddedNode.svelte
@@ -8,6 +8,9 @@
 	import Document from '$shell/Document.svelte';
 	import Pane from '$shell/Pane.svelte';
 	import Tabs from '$shell/Tabs.svelte';
+	import ButtonMenu from '$shell/ButtonMenu.svelte';
+	import IcoSave from '$shell/icons/IcoSave.svelte';
+	import ButtonMenuEntry from '$shell/ButtonMenuEntry.svelte';
 	import Content from '$shell/Content.svelte';
 	import Settings from '$shell/Settings.svelte';
 
@@ -16,7 +19,7 @@
 		save: (published: boolean) => Promise<void>;
 	};
 
-	let { node = $bindable() }: Props = $props();
+	let { node = $bindable(), save }: Props = $props();
 
 	let activeTab = $state('content');
 
@@ -35,6 +38,24 @@
 
 <Modal>
 	<div class="flex h-screen flex-col">
+		<div class="embed-control-bar sticky border-b border-gray-300 bg-white py-4">
+			<div class="mx-auto flex w-full max-w-7xl flex-row justify-end px-8">
+				<ButtonMenu
+					class="primary"
+					icon={IcoSave}
+					onclick={() => save(false)}
+					label={_('Speichern')}>
+					{#snippet children(closeMenu)}
+						<ButtonMenuEntry
+							onclick={() => {
+								save(true), closeMenu();
+							}}>
+							{_('Speichern und veröffentlichen')}
+						</ButtonMenuEntry>
+					{/snippet}
+				</ButtonMenu>
+			</div>
+		</div>
 		<Document>
 			<Tabs>
 				<button
