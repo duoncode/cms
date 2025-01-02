f28aaef8df1826d97b69ddb65a9316b7312ecbcd
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-01-02 16:27:54 +0100

    Translate PasswordTest from Pest to PHPUnit

--- tests/PasswordTest.php
+++ tests/PasswordTest.php
@@ -2,47 +2,54 @@
 
 declare(strict_types=1);
 
+namespace FiveOrbs\Cms\Tests;
+
 use FiveOrbs\Cms\Config;
 use FiveOrbs\Cms\Tests\Setup\TestCase;
 use FiveOrbs\Cms\Util\Password;
 
-uses(TestCase::class);
+final class PasswordTest extends TestCase
+{
+	public function testPasswordStrength(): void
+	{
+		$pw = new Password();
 
-test('Password strength', function () {
-	$pw = new Password();
-
-	expect($pw->strongEnough('1'))->toBe(false);
-	expect($pw->strongEnough('abcdef'))->toBe(false);
-	expect($pw->strongEnough('evil-chuck-666'))->toBe(true);
-});
-
-test('Password hash (default argon2)', function () {
-	$pw = new Password();
-
-	expect(str_starts_with($pw->hash('evil-chuck-666'), '$argon2id$v'))->toBe(true);
-});
-
-test('Password verify', function () {
-	$pw = new Password();
-	$hash = $pw->hash('evil-chuck-666');
-
-	expect($pw->valid('evil-chuck-666', $hash))->toBe(true);
-	expect($pw->valid('evil-chuck-660', $hash))->toBe(false);
-});
-
-test('Password init from config', function () {
-	$config = new Config('fiveorbs');
-	$hasArgon = Password::hasArgon2();
-
-	if ($hasArgon) {
-		$pw = Password::fromConfig($config);
-		expect(str_starts_with($pw->hash('evil-chuck-666'), '$argon2id$v'))->toBe(true);
+		$this->assertSame(false, $pw->strongEnough('1'));
+		$this->assertSame(false, $pw->strongEnough('abcdef'));
+		$this->assertSame(true, $pw->strongEnough('evil-chuck-666'));
 	}
 
-	$config->set('password.algorithm', PASSWORD_BCRYPT);
+	public function testPasswordHashDefaultArgon2(): void
+	{
+		$pw = new Password();
 
-	$pw = Password::fromConfig($this->config([
-		'password.algorithm' => PASSWORD_BCRYPT,
-	]));
-	expect(str_starts_with($pw->hash('evil-chuck-666'), '$2y$'))->toBe(true);
-});
+		$this->assertSame(true, str_starts_with($pw->hash('evil-chuck-666'), '$argon2id$v'));
+	}
+
+	public function testPasswordVerify(): void
+	{
+		$pw = new Password();
+		$hash = $pw->hash('evil-chuck-666');
+
+		$this->assertSame(true, $pw->valid('evil-chuck-666', $hash));
+		$this->assertSame(false, $pw->valid('evil-chuck-660', $hash));
+	}
+
+	public function testPasswordInitFromConfig(): void
+	{
+		$config = new Config('fiveorbs');
+		$hasArgon = Password::hasArgon2();
+
+		if ($hasArgon) {
+			$pw = Password::fromConfig($config);
+			$this->assertSame(true, str_starts_with($pw->hash('evil-chuck-666'), '$argon2id$v'));
+		}
+
+		$config->set('password.algorithm', PASSWORD_BCRYPT);
+
+		$pw = Password::fromConfig($this->config([
+			'password.algorithm' => PASSWORD_BCRYPT,
+		]));
+		$this->assertSame(true, str_starts_with($pw->hash('evil-chuck-666'), '$2y$'));
+	}
+}
