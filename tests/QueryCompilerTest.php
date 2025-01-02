8e09e5619463ba5f06c3f349a27e64e2d8881785
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-01-02 16:54:49 +0100

    Translate QueryCompilerTest from Pest to PHPUnit

--- tests/QueryCompilerTest.php
+++ tests/QueryCompilerTest.php
@@ -2,85 +2,110 @@
 
 declare(strict_types=1);
 
+namespace FiveOrbs\Cms\Tests;
+
 use FiveOrbs\Cms\Context;
 use FiveOrbs\Cms\Exception\ParserException;
 use FiveOrbs\Cms\Exception\ParserOutputException;
 use FiveOrbs\Cms\Finder\QueryCompiler;
 use FiveOrbs\Cms\Tests\Setup\TestCase;
 
-uses(TestCase::class);
+final class QueryCompilerTest extends TestCase
+{
+	private Context $context;
 
-beforeEach(function () {
-	$this->context = new Context(
-		$this->db(),
-		$this->request(),
-		$this->config(),
-		$this->registry(),
-	);
-});
+	protected function setup(): void
+	{
+		$this->context = new Context(
+			$this->db(),
+			$this->request(),
+			$this->config(),
+			$this->registry(),
+			$this->factory(),
+		);
+	}
 
-test('Simple AND query', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testSimpleAndQuery(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-	expect($compiler->compile('field=1 & builtin=2'))->toBe("n.content @@ '$.field.value == 1' AND builtin = 2");
-});
+		$this->assertSame(
+			"n.content @@ '$.field.value == 1' AND builtin = 2",
+			$compiler->compile('field=1 & builtin=2'),
+		);
+	}
 
-test('Simple OR query', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testSimpleOrQuery(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-	expect($compiler->compile('field=1 | builtin=2'))->toBe("n.content @@ '$.field.value == 1' OR builtin = 2");
-});
+		$this->assertSame(
+			"n.content @@ '$.field.value == 1' OR builtin = 2",
+			$compiler->compile('field=1 | builtin=2'),
+		);
+	}
 
-test('Nested query I', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testNestedQuery1(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-	expect($compiler->compile('field=1 & (builtin=2|builtin=3)'))->toBe(
-		"n.content @@ '$.field.value == 1' AND (builtin = 2 OR builtin = 3)",
-	);
-});
+		$this->assertSame(
+			"n.content @@ '$.field.value == 1' AND (builtin = 2 OR builtin = 3)",
+			$compiler->compile('field=1 & (builtin=2|builtin=3)'),
+		);
+	}
 
-test('Nested query II', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin', 'another' => 't.another']);
+	public function testNestedQuery2(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin', 'another' => 't.another']);
 
-	expect($compiler->compile("field=1 & (another='test'|(builtin>2 & builtin<5))"))->toBe(
-		"n.content @@ '$.field.value == 1' AND (t.another = 'test' OR (builtin > 2 AND builtin < 5))",
-	);
-});
+		$this->assertSame(
+			"n.content @@ '$.field.value == 1' AND (t.another = 'test' OR (builtin > 2 AND builtin < 5))",
+			$compiler->compile("field=1 & (another='test'|(builtin>2 & builtin<5))"),
+		);
+	}
 
-test('Nested query III', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin', 'another' => 't.another']);
+	public function testNestedQuery3(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin', 'another' => 't.another']);
 
-	expect($compiler->compile("(builtin = 1 | field=1) & (another='test'|(builtin>2 & builtin<5))"))->toBe(
-		"(builtin = 1 OR n.content @@ '$.field.value == 1')" .
-		' AND ' .
-		"(t.another = 'test' OR (builtin > 2 AND builtin < 5))",
-	);
-});
+		$this->assertSame(
+			"(builtin = 1 OR n.content @@ '$.field.value == 1')" .
+				' AND ' .
+				"(t.another = 'test' OR (builtin > 2 AND builtin < 5))",
+			$compiler->compile("(builtin = 1 | field=1) & (another='test'|(builtin>2 & builtin<5))"),
+		);
+	}
 
-test('Null query', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testNullQuery(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-	expect($compiler->compile('builtin = null'))->toBe(
-		'builtin IS NULL',
-	);
-});
+		$this->assertSame('builtin IS NULL', $compiler->compile('builtin = null'));
+	}
 
-test('Not Null query', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testNotNullQuery(): void
+	{
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-	expect($compiler->compile('builtin != null'))->toBe(
-		'builtin IS NOT NULL',
-	);
-});
+		$this->assertSame('builtin IS NOT NULL', $compiler->compile('builtin != null'));
+	}
 
-test('Null query wrong position', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+	public function testNullQueryWrongPosition(): void
+	{
+		$this->throws(ParserException::class, 'Parse error at position 1. Invalid position for a null value.');
 
-	$compiler->compile('null = builtin');
-})->throws(ParserException::class, 'Parse error at position 1. Invalid position for a null value.');
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
 
-test('Null query wrong operant', function () {
-	$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+		$compiler->compile('null = builtin');
+	}
 
-	$compiler->compile('builtin ~ null');
-})->throws(ParserOutputException::class, 'Only equal (=) or unequal (!=) operators are allowed');
+	public function testNullQueryWrongOperant(): void
+	{
+		$this->throws(ParserOutputException::class, 'Only equal (=) or unequal (!=) operators are allowed');
+
+		$compiler = new QueryCompiler($this->context, ['builtin' => 'builtin']);
+
+		$compiler->compile('builtin ~ null');
+	}
+}
