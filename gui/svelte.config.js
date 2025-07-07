645f3adc10013ec1e9266383f86b2d57e994c664
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-07-07 17:56:46 +0200

    Allow to specify built-time panel path at built-time

--- gui/svelte.config.js
+++ gui/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const panelPath = process.env.CMS_PANEL_PATH ? process.env.CMS_PANEL_PATH : 'cms';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		paths: { base: '/cms' },
+		paths: { base: `/${panelPath}` },
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
