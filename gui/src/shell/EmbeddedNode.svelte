4f9123b8ccc8415248f76571a270dbbdcc99e250
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-07-08 16:31:18 +0200

    Remove Modal component from EmbeddedNode

--- gui/src/shell/EmbeddedNode.svelte
+++ gui/src/shell/EmbeddedNode.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { Node } from '$types/data';
 
-	import Modal from '$shell/modal/Modal.svelte';
 	import { _ } from '$lib/locale';
 	import { system } from '$lib/sys';
 	import { dirty } from '$lib/state';
@@ -40,83 +39,80 @@
 	});
 </script>
 
-<Modal>
-	<div class="flex h-screen flex-col">
-		{#if fields.length === 0}
-			<div class="embed-control-bar sticky border-b border-gray-300 bg-white py-4">
-				<div class="mx-auto flex w-full max-w-7xl flex-row px-8">
-					<div
-						class="embed-status-bar flex flex-grow flex-row items-center justify-start">
-						<span class="inline-flex items-center">
-							<Published
-								published={node.published}
-								large />
+<div class="flex h-screen flex-col">
+	{#if fields.length === 0}
+		<div class="embed-control-bar sticky border-b border-gray-300 bg-white py-4">
+			<div class="mx-auto flex w-full max-w-7xl flex-row px-8">
+				<div class="embed-status-bar flex flex-grow flex-row items-center justify-start">
+					<span class="inline-flex items-center">
+						<Published
+							published={node.published}
+							large />
+					</span>
+					{#if $dirty}
+						<span
+							class="dirty-indicator ml-3 rounded-full bg-rose-600 px-2 pb-px text-sm font-bold text-white">
+							!
 						</span>
-						{#if $dirty}
-							<span
-								class="dirty-indicator ml-3 rounded-full bg-rose-600 px-2 pb-px text-sm font-bold text-white">
-								!
-							</span>
-						{/if}
-					</div>
-					<ButtonMenu
-						class="primary"
-						icon={IcoSave}
-						onclick={() => save(false)}
-						label={_('Speichern')}>
-						{#snippet children(closeMenu)}
-							<ButtonMenuEntry
-								onclick={() => {
-									save(true);
-									closeMenu();
-								}}>
-								{_('Speichern und veröffentlichen')}
-							</ButtonMenuEntry>
-						{/snippet}
-					</ButtonMenu>
+					{/if}
 				</div>
+				<ButtonMenu
+					class="primary"
+					icon={IcoSave}
+					onclick={() => save(false)}
+					label={_('Speichern')}>
+					{#snippet children(closeMenu)}
+						<ButtonMenuEntry
+							onclick={() => {
+								save(true);
+								closeMenu();
+							}}>
+							{_('Speichern und veröffentlichen')}
+						</ButtonMenuEntry>
+					{/snippet}
+				</ButtonMenu>
 			</div>
-		{/if}
-		<Document>
-			{#if fields.length > 0}
-				<Pane class="mt-6">
+		</div>
+	{/if}
+	<Document>
+		{#if fields.length > 0}
+			<Pane class="mt-6">
+				<Content
+					bind:fields={node.fields}
+					visibleFields={fields}
+					bind:node />
+			</Pane>
+			<div class="-mt-4 flex justify-end">
+				<Button onclick={() => save(true)}>
+					{_('Speichern')}
+				</Button>
+			</div>
+		{:else}
+			<Tabs>
+				<button
+					onclick={changeTab('content')}
+					class:active={activeTab === 'content'}
+					class="tab">
+					{_('Inhalt')}
+				</button>
+				{#if node.type.kind !== 'document'}
+					<button
+						onclick={changeTab('settings')}
+						class:active={activeTab === 'settings'}
+						class="tab">
+						{_('Einstellungen')}
+					</button>
+				{/if}
+			</Tabs>
+			<Pane>
+				{#if activeTab === 'content'}
 					<Content
 						bind:fields={node.fields}
-						visibleFields={fields}
 						bind:node />
-				</Pane>
-				<div class="-mt-4 flex justify-end">
-					<Button onclick={() => save(true)}>
-						{_('Speichern')}
-					</Button>
-				</div>
-			{:else}
-				<Tabs>
-					<button
-						onclick={changeTab('content')}
-						class:active={activeTab === 'content'}
-						class="tab">
-						{_('Inhalt')}
-					</button>
-					{#if node.type.kind !== 'document'}
-						<button
-							onclick={changeTab('settings')}
-							class:active={activeTab === 'settings'}
-							class="tab">
-							{_('Einstellungen')}
-						</button>
-					{/if}
-				</Tabs>
-				<Pane>
-					{#if activeTab === 'content'}
-						<Content
-							bind:fields={node.fields}
-							bind:node />
-					{:else}
-						<Settings bind:node />
-					{/if}
-				</Pane>
-			{/if}
-		</Document>
-	</div>
-</Modal>
+				{:else}
+					<Settings bind:node />
+				{/if}
+			</Pane>
+		{/if}
+	</Document>
+</div>
