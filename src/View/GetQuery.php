31e6c4359a6bfde7453bf0490509c5dbae0b804d
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-07-30 19:36:40 +0200

    Extract Query implementation properties into trait

--- src/View/GetQuery.php
+++ src/View/GetQuery.php
@@ -8,15 +8,7 @@ use Duon\Core\Request;
 
 class GetQuery implements Query
 {
-	private bool|null $_map;
-	private string|null $_query;
-	private bool|null $_published;
-	private bool|null $_hidden;
-	private bool|null $_deleted;
-	private bool|null $_content;
-	private array $_uids;
-	private string $_order;
-	private array $_fields;
+	use HasQueryProperties;
 
 	public function __construct(Request $request)
 	{
@@ -30,16 +22,6 @@ class GetQuery implements Query
 		$this->_order = $request->param('order', 'changed');
 		$this->_fields = explode(',', $request->param('fields', ''));
 	}
-
-	public bool|null $map { get => $this->_map; }
-	public string|null $query { get => $this->_query; }
-	public bool|null $published { get => $this->_published; }
-	public bool|null $hidden { get => $this->_hidden; }
-	public bool|null $deleted { get => $this->_deleted; }
-	public bool|null $content { get => $this->_content; }
-	public array $uids { get => $this->_uids; }
-	public string $order { get => $this->_order; }
-	public array $fields { get => $this->_fields; }
 
 	private function tristateValue(string|null $value): bool|null
 	{
--- src/View/PostQuery.php
+++ src/View/PostQuery.php
@@ -8,15 +8,7 @@ use Duon\Core\Request;
 
 class PostQuery implements Query
 {
-	private bool|null $_map;
-	private string|null $_query;
-	private bool|null $_published;
-	private bool|null $_hidden;
-	private bool|null $_deleted;
-	private bool|null $_content;
-	private array $_uids;
-	private string $_order;
-	private array $_fields;
+	use HasQueryProperties;
 
 	public function __construct(Request $request)
 	{
@@ -31,14 +23,4 @@ class PostQuery implements Query
 		$this->_order = $json['order'] ?? 'changed';
 		$this->_fields = $json['fields'] ?? [];
 	}
-
-	public bool|null $map { get => $this->_map; }
-	public string|null $query { get => $this->_query; }
-	public bool|null $published { get => $this->_published; }
-	public bool|null $hidden { get => $this->_hidden; }
-	public bool|null $deleted { get => $this->_deleted; }
-	public bool|null $content { get => $this->_content; }
-	public array $uids { get => $this->_uids; }
-	public string $order { get => $this->_order; }
-	public array $fields { get => $this->_fields; }
 }
