e9e7cedabea2b63fc63c973971c158efe3c25217
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-17 19:28:33 +0200

    Add logout action to panel navigation

--- panel/views/component/navigation.php
+++ panel/views/component/navigation.php
@@ -6,4 +6,8 @@
 	</ul>
 
 	<?php $this->insert('component/collections', ['level' => 1]) ?>
+
+	<form method="post" action="<?= $panelPath ?>/logout" hx-boost="false">
+		<button type="submit">Logout</button>
+	</form>
 </nav>
